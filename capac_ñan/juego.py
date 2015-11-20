3574648ef31e236726a9db2ee5e103173f10bdba
5t4t1ck <dsaavedra88@gmail.com>
2015-11-20 11:52:00 -0500

    creando la clase personaje

--- capac_ñan/juego.py
+++ capac_ñan/juego.py
@@ -5,22 +5,11 @@ pilas = pilasengine.iniciar()
 class personaje(pilasengine.actores.Actor):
 
     def iniciar(self):
-        self.imagen = "imagenes/corriendo.png"
-        self.y = 300
-        self.x = pilas.azar(-250, 250)
-        self.velocidad = 0
-        self.radio_de_colision = 50
-        self.escala = 0.70
-
+	self.imagen = "imagenes/corriendo.png"
+	
     def actualizar(self):
-        self.velocidad += 0.05
-        self.y -= self.velocidad
-        self.rotacion += 2
-
-        if self.y < -400:
-            self.eliminar()
+        pass
 
 wari = personaje(pilas)
-fondo = pilas.fondos.Fondo()
-#fondo.imagen = pilas.imagenes.cargar("imagenes/fondo.png")
+
 pilas.ejecutar()
