1c43643858d6de00344500758a211a4290252b26
Rommel Gutierrez Roa <ragutierrezroa@gmail.com>
2015-11-20 16:13:37 -0500

    movimiento de capac-ñan

--- capac_ñan/juego.py
+++ capac_ñan/juego.py
@@ -1,15 +1,26 @@
+#! /usr/bin/env python
+# -*- coding: utf-8 -*-
 import pilasengine
 
 pilas = pilasengine.iniciar()
 
-class personaje(pilasengine.actores.Actor):
+# Definimos las teclas que moverán al personaje
+#teclas = {pilas.simbolos.a:'izquierda', pilas.simbolos.s:'derecha'}
+# Creamos un control personalizado con esas teclas
+#mandos = pilas.control.Control(teclas)
+
+
+class Wari(pilasengine.actores.Actor):
 
     def iniciar(self):
-	self.imagen = "imagenes/corriendo.png"
-	
+        self.imagen = pilas.imagenes.cargar_grilla("imagenes/corriendo.png",16)
+        self.cuadro = 0
+        self.aprender(pilas.habilidades.MoverseConElTeclado)
+
     def actualizar(self):
         pass
 
-wari = personaje(pilas)
+pilas.actores.vincular(Wari)
 
+persona = pilas.actores.Wari()
 pilas.ejecutar()
